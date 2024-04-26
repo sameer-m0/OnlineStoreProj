28083cc7b0020143f6843b2577f9455e9df94280
sameerm-0 <sameerjm1508@gmail.com>
2024-04-26 17:27:37 +0530

    first commit

---
+++ src/main/java/com/shop/repository/RoleRepository.java
@@ -0,0 +1,9 @@
+package com.shop.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.shop.model.Role;
+
+public interface RoleRepository extends JpaRepository<Role, Integer> {
+
+}
